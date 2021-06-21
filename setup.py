1ddf3690972665c6e81c060d8d487d5b8ec7d5bf
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2021-06-22 01:53:01 +0200

    Fix make install script

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
     name='just-do-it-cli',
-    version='0.1.0',
+    version='0.1.1',
+    author='Ghassen Telmoudi',
+    author_email='ghassen.telmoudi@gmail.com',
     py_modules=['just_do_it_cli'],
     install_requires=[
-        'Click',
+        'Click==8.0.0',
+        'colorama==0.4.4',
+        'environs==9.3.2'
     ],
     entry_points={
         'console_scripts': [
